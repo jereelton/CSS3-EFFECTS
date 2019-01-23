246ebdc211483841d65c621c827335e53d394168
Jereelton de Oliveira Teixeira <jereelton@hotmail.com>
2019-01-22 22:20:36 -0200

    Transition Menu implemented, but without professional style :(

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,32 @@
 
-function MenuToogle() {
+function MenuToogle(box_id, menu_id, button_id) {
 
-  if(document.getElementById( 'ul_nav' ).style.height == 'auto') {
+	if(document.getElementById( box_id ).style.height == '211px') {
 
-    document.getElementById( 'ul_nav' ).style.height = '0px';
-    document.getElementById( 'button_nav_mobile' ).classList.remove('button_nav_menu--close');
-    document.getElementById( 'button_nav_mobile' ).classList.add('button_nav_menu--open');
+		// MENU CLOSE
 
-  } else {
+		document.getElementById( box_id ).style.height = '0px';
+		document.getElementById( box_id ).style.transition = 'all 600ms ease-out';
 
-    document.getElementById( 'ul_nav' ).style.height = 'auto';
-    document.getElementById( 'button_nav_mobile' ).classList.remove('button_nav_menu--open');
-    document.getElementById( 'button_nav_mobile' ).classList.add('button_nav_menu--close');
+		document.getElementById( button_id ).classList.remove('button_nav_menu--close');
+		document.getElementById( button_id ).classList.add('button_nav_menu--open');
 
-  }
+		document.getElementById( menu_id ).style.height = '0px';
+		document.getElementById( menu_id ).style.transition = 'all 600ms ease-out';
+
+	} else {
+
+		// MENU OPEN
+
+		document.getElementById( box_id ).style.height = '211px';
+		document.getElementById( box_id ).style.transition = 'all 600ms ease-out';
+
+		document.getElementById( button_id ).classList.remove('button_nav_menu--open');
+		document.getElementById( button_id ).classList.add('button_nav_menu--close');
+
+		document.getElementById( menu_id ).style.height = '211px';
+		document.getElementById( menu_id ).style.transition = 'all 600ms ease-out';
+
+	}
 
 }
